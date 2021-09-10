f5ea251427fb821c56535256a69c0b45acd99dc7
SkibskiyMax <max.skibskiy@gmail.com>
2021-09-10 17:24:10 +0300

    3 more solved

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,16 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let result = "";
+  members = members.filter(m => typeof(m) == 'string').map(x => x.toUpperCase().trim()).sort();
+
+  for (let i = 0; i < members.length; i++) {
+    result += members[i][0]
+  }
+
+  return result;
 }
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,54 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+    if (!Array.isArray(arr)) {
+      throw Error("'arr' parameter must be an instance of the Array!")
+    }
+
+    let array = Array.from(arr);
+    let result = [];
+
+    for (let i = 0; i < array.length; i++) {
+      switch(array[i]) {
+        case "--discard-next": {
+          if (array[i+1] !== undefined) {
+            array[i+1] = "DISCARDED"
+          } 
+          break;
+        }
+
+        case "--discard-prev": {
+          if (array[i-1] !== undefined  && array[i-1] !== "DISCARDED") {
+            array[i-1] = "DISCARDED"
+            result.pop();
+          }
+          break;
+        }
+
+        case "--double-next": {
+          if (array[i+1] !== undefined ) {
+            result.push(array[i+1]);
+          }
+
+          break;
+        }
+
+        case "--double-prev": {
+          if (array[i-1] !== undefined && array[i-1] !== "DISCARDED") {
+            result.push(array[i-1]);
+          }
+          break;
+        }
+        
+        case "DISCARDED": 
+          continue;
+
+        default: {
+          result.push(array[i]);
+          continue;
+        };
+      }
+    }
+    return result;
 }
